e84abea7278a1ede9aba9691acd97ed6182dc3c4
Jonathan Louis Kaplan <jonathanlouiskaplan@outlook.com>
2021-07-11 15:50:50 +0100

    ex 1 44 done

--- chapter01/ex_1-43.rkt
+++ chapter01/ex_1-43.rkt
@@ -10,12 +10,13 @@
 (define (square x)
   (* x x))
 
+; tail call recursive
 (define (repeated funco n)
-  (define (rep-rec funci n)
+  (define (rep-iter funci n)
     (if (= n 1)
         funci
-        (rep-rec (compose funco funci) (- n 1))))
-  (rep-rec funco n))
+        (rep-iter (compose funco funci) (- n 1))))
+  (rep-iter funco n))
 
 ((repeated square 2) 5)
 ((repeated inc 9) 0)
