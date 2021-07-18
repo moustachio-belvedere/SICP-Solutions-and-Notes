cc476770d218cadcb30adaf517f34e09d5ce6ad0
Jonathan Louis Kaplan <jonathanlouiskaplan@outlook.com>
2021-07-18 14:04:25 +0100

    ex 2 01 done

---
+++ chapter02/ex_2-01.rkt
@@ -0,0 +1,63 @@
+#lang sicp
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))))
+
+(define (equal-rat? x y)
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
+
+(define (numer x) (car x))
+(define (denom x) (cdr x))
+
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
+
+(define (gcd a b)
+  (if (= b 0)
+    a
+    (gcd b (remainder a b))))
+
+(define (make-rat n d)
+  (let ((g (gcd n d)))
+    (cons (/ n g) 
+          (/ d g))))
+
+(define x (make-rat -8 -4))
+(print-rat x)
+(newline)
+(display (car x))
+(newline)
+(display (cdr x))
+
+(define y (make-rat 8 -4))
+(print-rat y)
+(newline)
+(display (car y))
+(newline)
+(display (cdr y))
+
+(define z (make-rat 8 -4))
+(print-rat z)
+(newline)
+(display (car z))
+(newline)
+(display (cdr z))
