6d8836d3caeb0bcb48b30f931f0d68f375a52547
Jonathan Louis Kaplan <jonathanlouiskaplan@outlook.com>
2021-07-25 17:33:28 +0100

    ex 2 05 neaten sol

--- chapter02/ex_2-05.rkt
+++ chapter02/ex_2-05.rkt
@@ -1,30 +1,23 @@
 #lang sicp
 
-(define (not-div-by-2 x)
+(define (not-div-by-y x y)
   (not (= 0
-          (remainder x 2))))
+          (remainder x y))))
 
-(define (not-div-by-3 x)
-  (not (= 0
-          (remainder x 3))))
+(define (get-log-y-inv x y)
+  (inexact->exact (/ (log x) (log y))))
 
 (define (cons_alt x y)
   (* (expt 2 x) (expt 3 y)))
 
-(define (get-log-2-inv x)
-  (inexact->exact (/ (log x) (log 2))))
-
-(define (get-log-3-inv x)
-  (inexact->exact (/ (log x) (log 3))))
-
 (define (car_alt z)
-  (if (not-div-by-3 z)
-      (get-log-2-inv z)
+  (if (not-div-by-y z 3)
+      (get-log-y-inv z 2)
       (car_alt (/ z 3))))
 
 (define (cdr_alt z)
-  (if (not-div-by-2 z)
-      (get-log-3-inv z)
+  (if (not-div-by-y z 2)
+      (get-log-y-inv z 3)
       (cdr_alt (/ z 2))))
 
 (define x (cons_alt 7 11))
