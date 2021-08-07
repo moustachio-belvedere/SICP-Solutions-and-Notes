82cac125b25d42ff043bb10d6d038ab10870d192
Jonathan Louis Kaplan <jonathanlouiskaplan@outlook.com>
2021-08-07 17:44:21 +0100

    ex 2 06 done

--- chapter02/ex_2-06.rkt
+++ chapter02/ex_2-06.rkt
@@ -5,7 +5,24 @@
 (define (add-1 n)
   (lambda (f) (lambda (x) (f ((n f) x)))))
 
-((zero 0) 1)
-((zero 0) 2)
-((zero 0) 3)
-((zero 0) 4)
+; define 1 via substitution
+; (add-1 0)
+; (lambda (f) (lambda (x) (f (((lambda (f) (lambda (x) x)) f) x))))
+; (lambda (f) (lambda (x) (f x)))
+(define one (lambda (f) (lambda (x) (f x))))
+
+; following same substitution process using
+; (add-1 one)
+; yields
+(define two (lambda (f) (lambda (x) (f (f x)))))
+
+; noticing the pattern that composed applications of `f` corresponds to the integer,
+; seek functions that compose as required. Exploring 1 + 1
+; A: ((lambda (f) (lambda (x) (f x))) f) = (lambda (x) (f x))
+; B: (((lambda (f) (lambda (x) (f x))) f) x) = (f x)
+; Applying B as operand to function A yields
+; C: ((lambda (x) (f x)) (f x)) = (f (f x))
+; which is the required body of the 2 integer function.
+; generalising this yields:
+(define (add a b)
+  (lambda (f) (lambda (x) ((a f) ((b f) x)))))
