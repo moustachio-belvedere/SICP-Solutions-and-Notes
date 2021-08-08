5b23cc7fce5b3c95344f304e0fe7855a7f0725a8
Jonathan Louis Kaplan <jonathanlouiskaplan@outlook.com>
2021-08-08 10:28:16 +0100

    ex 2 09 done

--- chapter02/ex_2-09.rkt
+++ chapter02/ex_2-09.rkt
@@ -1,3 +1,19 @@
 #lang sicp
 
+; w0 = (u0 - l0)/2
+; w1 = (u1 - l1)/2
+ 
+;   w_sum
+; = ((u0 + u1) - (l0 + l1))/2
+; = (u0 + u1)/2 - (l0 + l1)/2
+; = (u0 - l0)/2 + (u1 - l1)/2
+; = w0 + w1
+
+; very similar derivation for subtraction
+
+; an example of when it doesn't work for mutliplication (follow similar path for division proof)
+;   w_prod
+; = (u0*u1 - l0*l1)/2
+
+; which cannot be rearranged to form w0 and w1 separately
 
