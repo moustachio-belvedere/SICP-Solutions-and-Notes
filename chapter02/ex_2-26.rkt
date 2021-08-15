530e2506e133a8203787e16a2ed0a9245a4375ba
Jonathan Louis Kaplan <jonathanlouiskaplan@outlook.com>
2021-08-15 14:53:49 +0100

    ex 2 26 done

---
+++ chapter02/ex_2-26.rkt
@@ -0,0 +1,23 @@
+#lang sicp
+
+(define (append list1 list2)
+  (if (null? list1)
+      list2
+      (cons (car list1) 
+            (append (cdr list1) 
+                    list2))))
+
+(define x (list 1 2 3))
+(define y (list 4 5 6))
+
+; guess: (1 2 3 4 5 6)
+(append x y)
+; correct
+
+; guess ((1 2 3) (4 5 6)
+(cons x y)
+; NO. ((1 2 3) 4 5 6)
+
+; guess ((1 2 3) (4 5 6))
+(list x y)
+; correct
