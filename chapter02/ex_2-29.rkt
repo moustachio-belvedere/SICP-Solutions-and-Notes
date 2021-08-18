bf367ebb7519041f9896c67e3954f10d57c1566b
Jonathan Louis Kaplan <jonathanlouiskaplan@outlook.com>
2021-08-18 18:03:12 +0100

    started ex 2 29 b

--- chapter02/ex_2-29.rkt
+++ chapter02/ex_2-29.rkt
@@ -1,5 +1,32 @@
 #lang sicp
 
+(define (append list1 list2)
+  (if (null? list1)
+      list2
+      (cons (car list1) 
+            (append (cdr list1) 
+                    list2))))
+
+(define (map proc items)
+  (if (null? items)
+      nil
+      (cons (proc (car items))
+            (map proc (cdr items)))))
+
+(define (flatten-once items)
+  (define (flat-iter items acc)
+    (cond ((null? items) acc)
+          ((pair? (car items)) (flat-iter (cdr items) (append acc (car items))))
+          (else (flat-iter (cdr items) (append acc (list (car items)))))))
+  (flat-iter items nil))
+
+(define (fringe items)
+  (if (pair? items)
+      (flatten-once (map fringe items))
+      items))
+
+(define (sum list) (apply + list))
+
 (define (make-mobile left right)
   (list left right))
 
@@ -18,3 +45,4 @@
 (define (branch-structure branch)
   (cadr branch))
 
+(sum (list 1 2 3 4 5))
