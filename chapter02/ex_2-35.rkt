ff779c0cafee58aa5a7cb0be48cacc44d9cf0775
Jonathan Louis Kaplan <jonathanlouiskaplan@outlook.com>
2021-08-21 15:52:15 +0100

    started 2 35 and 46

---
+++ chapter02/ex_2-35.rkt
@@ -0,0 +1,35 @@
+#lang sicp
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op 
+                      initial 
+                      (cdr sequence)))))
+
+(define (filter predicate sequence)
+  (cond ((null? sequence) nil)
+        ((predicate (car sequence))
+         (cons (car sequence)
+               (filter predicate 
+                       (cdr sequence))))
+        (else  (filter predicate 
+                       (cdr sequence)))))
+
+(define (enumerate-tree tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (list tree))
+        (else (append 
+               (enumerate-tree (car tree))
+               (enumerate-tree (cdr tree))))))
+
+(define (count-leaves t)
+  (accumulate (lambda (x y) (+ 1 y))
+              0
+              (enumerate-tree t)))
+
+; 10 leaves
+(define x (list 9 5 2 (list 1 2 (list 5 3) 3 2) 1))
+
+(count-leaves x)
---
+++ chapter02/ex_2-36.rkt
@@ -0,0 +1,19 @@
+#lang sicp
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op 
+                      initial 
+                      (cdr sequence)))))
+
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      nil
+      (cons (accumulate op init (list 1 2 3)))
+            (accumulate-n op init (cdr seqs)))))
+
+(define x (list (list 1 2 3) (list 4 5 6) (list 7 8 9)))
+
+(accumulate-n + 0 x)
