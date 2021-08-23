6027d3c1270042ef407afd84db91b0baca478466
Jonathan Louis Kaplan <jonathanlouiskaplan@outlook.com>
2021-08-23 17:35:16 +0100

    ex 2 35 done

--- chapter02/ex_2-35.rkt
+++ chapter02/ex_2-35.rkt
@@ -8,39 +8,17 @@
                       initial 
                       (cdr sequence)))))
 
-(define (filter predicate sequence)
-  (cond ((null? sequence) nil)
-        ((predicate (car sequence))
-         (cons (car sequence)
-               (filter predicate 
-                       (cdr sequence))))
-        (else  (filter predicate 
-                       (cdr sequence)))))
-
-(define (enumerate-tree tree)
-  (cond ((null? tree) nil)
-        ((not (pair? tree)) (list tree))
-        (else (append 
-               (enumerate-tree (car tree))
-               (enumerate-tree (cdr tree))))))
-
-(define (enum-t x)
-  (map (lambda (x) (if (pair? x)
-                       (map (lambda (y) 
-                       (append (list (car y) (enum-t (cdr x))))
-                       x))
-       x))
-
 (define (count-leaves t)
-  (accumulate (lambda (x y) (+ 1 y))
+  (accumulate (lambda (x y) (+ x y))
               0
-              (enumerate-tree t)))
+              (map (lambda (x) (if (pair? x)
+                                 (count-leaves x)
+                                 1))
+                   t)))
 
 ; 10 leaves
 (define x (list 9 5 2 (list 1 2 (list 5 3) 3 2) 1))
 
 (display x)
 (newline)
-(enum-t x)
-(enumerate-tree x)
 (count-leaves x)
