d0f8c8438b36dcc1058b7760cb9107d8b5b3c5da
Jonathan Louis Kaplan <jonathanlouiskaplan@outlook.com>
2021-08-23 21:41:54 +0100

    progress on ex 2 38

--- chapter02/ex_2-38.rkt
+++ chapter02/ex_2-38.rkt
@@ -1,1 +1,19 @@
 #lang sicp
+
+; (fold-right / 1 (list 1 2 3)) =
+; (((3/2)/1)/1) = 
+; 3/2 
+
+; (fold-left / 1 (list 1 2 3)) = 
+; (((1/2)/3)/1) = 
+; 1/6
+
+; (fold-right list nil (list 1 2 3)) = 
+; (list (list (list 3) 2) 1) =
+; (((3) 2) 1)
+
+; (fold-left list nil (list 1 2 3)) =
+; (list (list (list 1) 2) 3) =
+; (((1) 2) 3)
+
+; for fold-right and fold-left to be guaranteed equal, the `op` must be commutable 
