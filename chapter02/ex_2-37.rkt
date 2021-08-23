b46fcad04472ff1359dee10142494890f9b8f16c
Jonathan Louis Kaplan <jonathanlouiskaplan@outlook.com>
2021-08-23 21:04:14 +0100

    ex 2 38 done

--- chapter02/ex_2-37.rkt
+++ chapter02/ex_2-37.rkt
@@ -18,8 +18,24 @@
   (accumulate + 0 (map * v w)))
 
 (define (matrix-*-vector m v)
-  (map ... m))
+  (map (lambda (x) (dot-product v x)) m))
+
+(define (transpose mat)
+  (accumulate-n cons nil mat))
+
+(define (matrix-*-matrix m n)
+  (let ((cols (transpose n)))
+    (map (lambda (x) (matrix-*-vector cols x)) m)))
 
 (define x (list (list 1 2 3 4)
                 (list 4 5 6 6)
                 (list 6 7 8 9)))
+
+(define y (list (list 1 2)
+                (list 3 4)))
+
+(define z (list 5 7))
+
+(matrix-*-vector y z)
+(transpose x)
+(matrix-*-matrix y y)
