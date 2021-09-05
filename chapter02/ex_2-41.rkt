f56ef075e962a049a25d846d6fbdf88ff9a2e9fc
Jonathan Louis Kaplan <jonathanlouiskaplan@outlook.com>
2021-09-06 00:52:48 +0100

    progress on ex 2 41

--- chapter02/ex_2-41.rkt
+++ chapter02/ex_2-41.rkt
@@ -1,26 +1,5 @@
 #lang sicp
 
-(define (square x)
-  (* x x))
-
-(define (smallest-divisor n)
-  (find-divisor n 2))
-
-(define (find-divisor n test-divisor)
-  (cond ((> (square test-divisor) n) 
-         n)
-        ((divides? test-divisor n) 
-         test-divisor)
-        (else (find-divisor 
-               n 
-               (+ test-divisor 1)))))
-
-(define (divides? a b)
-  (= (remainder b a) 0))
-
-(define (prime? n)
-  (= n (smallest-divisor n)))
-
 (define (enumerate-interval low high)
   (if (> low high)
       nil
@@ -47,14 +26,6 @@
 (define (flatmap proc seq)
   (accumulate append nil (map proc seq)))
 
-(define (prime-sum? pair)
-  (prime? (+ (car pair) (cadr pair))))
-
-(define (make-pair-sum pair)
-  (list (car pair) 
-        (cadr pair) 
-        (+ (car pair) (cadr pair))))
-
 (define (filter predicate sequence)
   (cond ((null? sequence) nil)
         ((predicate (car sequence))
@@ -66,32 +37,27 @@
 
 (define (unique-pairs n)
   (flatmap
-   (lambda (i)
-      (map (lambda (j) 
-        (list i j))
-      (enumerate-interval 1 (- i 1))))
-  (enumerate-interval 1 n)))
+    (lambda (i)
+      (map (lambda (j)
+             (list i j))
+           (enumerate-interval 1 (- i 1))))
+    (enumerate-interval 1 n)))
+
+(define (get-trip i)
+  (map (lambda (x)
+         (append (list i) x))
+  (unique-pairs (- i 1))))
 
 (define (unique-triplets n)
-  (flatmap
-    (lambda (i
+  (flatmap get-trip
+           (enumerate-interval 3 n)))
 
-(define (prime-sum-pairs-book n)
-  (map make-pair-sum
-       (filter 
-        prime-sum?
-        (flatmap
-         (lambda (i)
-           (map (lambda (j) 
-                  (list i j))
-                (enumerate-interval 
-                 1 
-                 (- i 1))))
-         (enumerate-interval 1 n)))))
+(unique-triplets 4)
 
-(define (prime-sum-pairs n)
-  (map make-pair-sum
-       (filter 
-        prime-sum?
-        (unique-pairs n))))
+(define (sumeqto-s? x s)
+  (= (apply + x) s))
 
+(define (triples-sum-to-s s n)
+  (filter sumeqto-s? (unique-triplets n)))
+
+; first consider unordered triplets, then consider ordered triplets
