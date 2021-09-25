08540f571117225dd41d897aaa45d77fed53bb9e
Jonathan Louis Kaplan <jonathanlouiskaplan@outlook.com>
2021-09-25 19:27:24 +0100

    ex 2 56 done

--- chapter02/ex_2-56.rkt
+++ chapter02/ex_2-56.rkt
@@ -1,6 +1,20 @@
 #lang sicp
 (#%require "utils_symdiff.rkt")
 
+(define (make-exponentiation b e)
+  (cond ((=number? e 0) 1)
+        ((=number? b 0) 0)
+        ((=number? e 1) b)
+        ((and (number? b) (number? e))
+         (expt b e))
+        (else (list `** b e))))
+
+(define (base x) (cadr x))
+(define (exponent x) (caddr x))
+
+(define (exponentiation? x)
+  (and (pair? x) (eq? (car x) `**)))
+
 (define (deriv exp var)
   (cond ((number? exp) 0)
         ((variable? exp)
@@ -16,5 +30,18 @@
           (make-product 
            (deriv (multiplier exp) var)
            (multiplicand exp))))
+        ((exponentiation? exp)
+         (make-product
+           (exponent exp)
+           (make-product
+             (make-exponentiation (base exp)
+                                  (- (exponent exp) 1))
+             (deriv (base exp) var))))
         (else (error "unknown expression 
                       type: DERIV" exp))))
+
+(define u (make-sum (make-product `x 2)
+                    1))
+(define un (make-exponentiation u 2))
+(deriv un `x)
+
