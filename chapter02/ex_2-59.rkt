56babe1f089c5863a151174365446e1ccb44b598
Jonathan Louis Kaplan <j.kiszle@gmail.com>
2021-10-12 20:35:58 +0100

    done 2 60, neatened 2 59

--- chapter02/ex_2-59.rkt
+++ chapter02/ex_2-59.rkt
@@ -2,7 +2,7 @@
 (#%require "utils_sets.rkt")
 
 (define (union-set s1 s2)
-  (cond ((or (null? s1)) s2)
+  (cond ((null? s1) s2)
         ((element-of-set? (car s1) s2)
             (union-set (cdr s1) s2))
         (else (union-set (cdr s1) (cons (car s1) s2)))))
