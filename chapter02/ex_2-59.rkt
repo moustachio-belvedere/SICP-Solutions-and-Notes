03d68993d382e8f8e41489700041d8ac37ec0064
Jonathan Louis Kaplan <j.kiszle@gmail.com>
2021-10-12 19:48:17 +0100

    ex 2 59 done

---
+++ chapter02/ex_2-59.rkt
@@ -0,0 +1,13 @@
+#lang sicp
+(#%require "utils_sets.rkt")
+
+(define (union-set s1 s2)
+  (cond ((or (null? s1)) s2)
+        ((element-of-set? (car s1) s2)
+            (union-set (cdr s1) s2))
+        (else (union-set (cdr s1) (cons (car s1) s2)))))
+
+(define x (list `x `y `z 2 3 5))
+(define y (list `a `b `c 2 7))
+
+(union-set x y)
