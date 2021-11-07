063a88a3d6527dbb8141a9b1169f292c01b2971b
Jonathan Louis Kaplan <j.kiszle@gmail.com>
2021-11-07 17:48:51 +0000

    done ex 2 81

---
+++ chapter02/utils_typecoerce.rkt
@@ -0,0 +1,36 @@
+#lang sicp
+(#%provide get-coercion put-coercion)
+
+(define (make-table)
+  (let ((local-table (list '*table*)))
+    (define (lookup key-1 key-2)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+              (if record
+                  (cdr record)
+                  false))
+            false)))
+    (define (insert! key-1 key-2 value)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+              (if record
+                  (set-cdr! record value)
+                  (set-cdr! subtable
+                            (cons (cons key-2 value)
+                                  (cdr subtable)))))
+            (set-cdr! local-table
+                      (cons (list key-1
+                                  (cons key-2 value))
+                            (cdr local-table)))))
+      'ok)    
+    (define (dispatch m)
+      (cond ((eq? m 'lookup-proc) lookup)
+            ((eq? m 'insert-proc!) insert!)
+            (else (error "Unknown type - TABLE" m))))
+    dispatch))
+
+(define type-table (make-table))
+(define get-coercion (type-table 'lookup-proc))
+(define put-coercion (type-table 'insert-proc!))
