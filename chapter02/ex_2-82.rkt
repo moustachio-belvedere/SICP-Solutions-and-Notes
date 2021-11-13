15e98801c252a25caccef7179a83883cd06de46c
Jonathan Louis Kaplan <j.kiszle@gmail.com>
2021-11-13 13:49:52 +0000

    simplify ex 2 82 for start to address

--- chapter02/ex_2-82.rkt
+++ chapter02/ex_2-82.rkt
@@ -19,8 +19,8 @@
         (else (error "Bad tagged datum: 
               CONTENTS" datum))))
 
-(define (coercer types)
-  (define (coerce-iter typeholder 
+;(define (coercer types)
+;  (define (coerce-iter curtype 
 
 (define (apply-generic op . args)
   (let ((type-tags (map type-tag args)))
@@ -59,42 +59,18 @@
 
 (define (square x) (* x x))
 (define (add x y) (apply-generic 'add x y))
-(define (sub x y) (apply-generic 'sub x y))
-(define (mul x y) (apply-generic 'mul x y))
-(define (div x y) (apply-generic 'div x y))
-(define (equ? x y) (apply-generic 'equ? x y))
-(define (=zero? x) (apply-generic '=zero? x))
 
-;; --------------
-;; SCHEME NUMBERS
-;; --------------
 (define (install-scheme-number-package)
   (define (tag x)
     (attach-tag 'scheme-number x))
+  (put 'bigadd '(list scheme-number)
+       (lambda (x) (apply + x)))
   (put 'add '(scheme-number scheme-number)
        (lambda (x y) (tag (+ x y))))
-  (put 'sub '(scheme-number scheme-number)
-       (lambda (x y) (tag (- x y))))
-  (put 'mul '(scheme-number scheme-number)
-       (lambda (x y) (tag (* x y))))
-  (put 'div '(scheme-number scheme-number)
-       (lambda (x y) (tag (/ x y))))
-  (put 'equ? '(scheme-number scheme-number)
-       (lambda (x y) (= x y)))
-  (put '=zero? '(scheme-number)
-       (lambda (x) (= x 0)))
   (put 'make 'scheme-number
        (lambda (x) (tag x)))
   'done)
 
-(define (make-scheme-number n)
-  ((get 'make 'scheme-number) n))
-
-(install-scheme-number-package)
-
-;; ----------------
-;; RATIONAL NUMBERS
-;; ----------------
 (define (install-rational-package)
   ;; internal procedures
   (define (numer x) (car x))
@@ -106,43 +82,14 @@
     (make-rat (+ (* (numer x) (denom y))
                  (* (numer y) (denom x)))
               (* (denom x) (denom y))))
-  (define (sub-rat x y)
-    (make-rat (- (* (numer x) (denom y))
-                 (* (numer y) (denom x)))
-              (* (denom x) (denom y))))
-  (define (mul-rat x y)
-    (make-rat (* (numer x) (numer y))
-              (* (denom x) (denom y))))
-  (define (div-rat x y)
-    (make-rat (* (numer x) (denom y))
-              (* (denom x) (numer y))))
   ;; interface to rest of the system
   (define (tag x) (attach-tag 'rational x))
   (put 'add '(rational rational)
        (lambda (x y) (tag (add-rat x y))))
-  (put 'sub '(rational rational)
-       (lambda (x y) (tag (sub-rat x y))))
-  (put 'mul '(rational rational)
-       (lambda (x y) (tag (mul-rat x y))))
-  (put 'div '(rational rational)
-       (lambda (x y) (tag (div-rat x y))))
-  (put 'equ? '(rational rational)
-       (lambda (x y) (and (= (numer x) (numer y))
-                          (= (denom x) (denom y)))))
-  (put '=zero? '(rational)
-       (lambda (x) (= (numer x) 0)))
   (put 'make 'rational
        (lambda (n d) (tag (make-rat n d))))
   'done)
 
-(define (make-rational n d)
-  ((get 'make 'rational) n d))
-
-(install-rational-package)
-
-;; ---------------
-;; COMPLEX NUMBERS
-;; ---------------
 (define (install-rectangular-package)
   ;; internal procedures
   (define (real-part z) (car z))
@@ -198,8 +145,6 @@
   'done)
 
 (define (install-complex-package)
-  ;; imported procedures from rectangular 
-  ;; and polar packages
   (define (make-from-real-imag x y)
     ((get 'make-from-real-imag 
           'rectangular) 
@@ -216,43 +161,12 @@
     (apply-generic 'magnitude z))
   (define (angle z) 
     (apply-generic 'angle z))
+  (define (tag x) (attach-tag 'complex x))
 
   (define (add-complex z1 z2)
     (make-from-real-imag 
      (+ (real-part z1) (real-part z2))
      (+ (imag-part z1) (imag-part z2))))
-  (define (sub-complex z1 z2)
-    (make-from-real-imag 
-     (- (real-part z1) (real-part z2))
-     (- (imag-part z1) (imag-part z2))))
-  (define (mul-complex z1 z2)
-    (make-from-mag-ang 
-     (* (magnitude z1) (magnitude z2))
-     (+ (angle z1) (angle z2))))
-  (define (div-complex z1 z2)
-    (make-from-mag-ang 
-     (/ (magnitude z1) (magnitude z2))
-     (- (angle z1) (angle z2))))
-  ;; interface to rest of the system
-  (define (tag z) (attach-tag 'complex z))
-  (put 'add '(complex complex)
-       (lambda (z1 z2) 
-         (tag (add-complex z1 z2))))
-  (put 'sub '(complex complex)
-       (lambda (z1 z2) 
-         (tag (sub-complex z1 z2))))
-  (put 'mul '(complex complex)
-       (lambda (z1 z2) 
-         (tag (mul-complex z1 z2))))
-  (put 'div '(complex complex)
-       (lambda (z1 z2) 
-         (tag (div-complex z1 z2))))
-  (put 'equ? '(complex complex)
-       (lambda (x y) (and (= (real-part x) (real-part y))
-                          (= (imag-part x) (imag-part y)))))
-  (put '=zero? '(complex)
-       (lambda (x) (and (= (real-part x) 0)
-                        (= (imag-part x) 0)))) ; could use magnitude instead
   (put 'make-from-real-imag 'complex
        (lambda (x y)
          (tag (make-from-real-imag x y))))
@@ -266,23 +180,20 @@
 
   'done)
 
+(install-scheme-number-package)
+(install-rational-package)
 (install-rectangular-package)
 (install-polar-package)
 (install-complex-package)
 
+(define (make-scheme-number n)
+  ((get 'make 'scheme-number) n))
+
+(define (make-rational n d)
+  ((get 'make 'rational) n d))
+
 (define (make-complex-from-real-imag x y)
   ((get 'make-from-real-imag 'complex) x y))
+
 (define (make-complex-from-mag-ang r a)
   ((get 'make-from-mag-ang 'complex) r a))
-
-;; a) Louis Reasoner has bad reasoning. His addition
-;; will lead to an infinite loop if the requested 
-;; operation is not defined in the operation table.
-
-;; b) However, Louis is correct that something
-;; needed to be done, just wrong about what specifically
-;; needed changing. If both arguments are of
-;; same type, we could promote BOTH to a 'higher type'
-;; to test if there defined binary ops on that higher
-;; type, or we just raise error to indicate no op
-;; available. Book suggests latter for part c).
