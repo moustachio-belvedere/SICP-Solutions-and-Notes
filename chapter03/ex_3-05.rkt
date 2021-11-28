00a1598aa96c04364a376a16b0fd2b119ffe7f0d
Jonathan Louis Kaplan <j.kiszle@gmail.com>
2021-11-28 23:45:49 +0000

    start ex 3 05, add random utils

---
+++ chapter03/ex_3-05.rkt
@@ -0,0 +1,23 @@
+#lang sicp
+(#%require "utils_random.rkt")
+
+(define (estimate-pi trials)
+  (sqrt (/ 6 (monte-carlo trials 
+                          cesaro-test))))
+
+(define (cesaro-test)
+   (= (gcd (rand) (rand)) 1))
+
+(define (monte-carlo trials experiment)
+  (define (iter trials-remaining trials-passed)
+    (cond ((= trials-remaining 0)
+           (/ trials-passed trials))
+          ((experiment)
+           (iter (- trials-remaining 1) 
+                 (+ trials-passed 1)))
+          (else
+           (iter (- trials-remaining 1) 
+                 trials-passed))))
+  (iter trials 0))
+
+(estimate-pi 1000000)
