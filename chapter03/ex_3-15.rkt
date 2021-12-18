a07551f70c4b30c22c6d7d9f0c5ddc1338d4a5ac
Jonathan Louis Kaplan <j.kiszle@gmail.com>
2021-12-18 16:21:12 +0000

    ex 3 15 started

---
+++ chapter03/ex_3-15.rkt
@@ -0,0 +1,13 @@
+#!/bin/racket
+#lang sicp
+
+(define (set-to-wow! x)
+  (set-car! (car x) 'wow)
+  x)
+
+(define x (list 'a 'b))
+(define z1 (cons x x))
+(define z2 
+  (cons (list 'a 'b) (list 'a 'b)))
+
+
