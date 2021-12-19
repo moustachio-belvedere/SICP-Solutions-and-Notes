2776d954208f6b0d64a68a515812e0f07ea60bfa
Jonathan Louis Kaplan <j.kiszle@gmail.com>
2021-12-19 23:06:56 +0000

    some sdraw exploration

--- chapter03/ex_plore_sdraw.rkt
+++ chapter03/ex_plore_sdraw.rkt
@@ -3,10 +3,19 @@
 (require pict)
 (require sdraw)
 
-(define ltest (sdraw '(1 2) #:null-style '/ #:null-thickness 0.5))
-
 (define (save-pict the-pict name kind)
   (define bm (pict->bitmap the-pict))
   (send bm save-file name kind))
 
-(save-pict ltest "ltest.png" 'png)
+;; (define ltest (sdraw '(1 2) #:null-style '/ #:null-thickness 0.5))
+
+(define x (list 'a 'b))
+(define z1 (cons x x))
+(define z2 
+  (cons (list 'a 'b) (list 'a 'b)))
+
+(define z1pic (sdraw z1 #:null-style '/ #:null-thickness 0.5))
+(define z2pic (sdraw z2 #:null-style '/ #:null-thickness 0.5))
+
+(save-pict z1pic "z1pic.png" 'png)
+(save-pict z2pic "z2pic.png" 'png)
