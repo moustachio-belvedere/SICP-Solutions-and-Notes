421b04e3f45a0f443d8b0e46d049c9d3d785a474
Jonathan Louis Kaplan <j.kiszle@gmail.com>
2022-07-16 15:11:30 +0100

    done ex 4 26

---
+++ chapter04/ex_4-26_example.rkt
@@ -0,0 +1,4 @@
+;; "give an example of a situation where it might be useful to have unless available as a procedure rather than as special form."
+
+;; only examples came across where `map` functional-style iterators are used.
+
