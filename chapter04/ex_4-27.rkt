6ca622df394cfb9cdd3f624a34975c7fdc6da303
Jonathan Louis Kaplan <j.kiszle@gmail.com>
2022-07-17 18:21:55 +0100

    ex 4 27, lazy evaluator ready

--- chapter04/ex_4-27.rkt
+++ chapter04/ex_4-27.rkt
@@ -41,35 +41,49 @@
 (define (actual-value exp env)
   (force-it (eval exp env)))
 
-(define (metapply procedure arguments)
+(define (metapply procedure arguments env)
   (cond ((primitive-procedure? procedure)
          (apply-primitive-procedure
           procedure
-          arguments))
+          (list-of-arg-values
+           arguments
+           env)))  ; changed
         ((compound-procedure? procedure)
          (eval-sequence
-           (procedure-body procedure)
-           (extend-environment
-             (procedure-parameters
-              procedure)
-             arguments
-             (procedure-environment
-              procedure))))
-        (else
-         (error "Unknown procedure
-                 type: APPLY"
-                procedure))))
+          (procedure-body procedure)
+          (extend-environment
+           (procedure-parameters procedure)
+           (list-of-delayed-args
+            arguments
+            env)   ; changed
+           (procedure-environment procedure))))
+        (else (error "Unknown procedure
+                      type: APPLY"
+                     procedure))))
 
-(define (list-of-values exps env)
+(define (list-of-arg-values exps env)
   (if (no-operands? exps)
       '()
-      (cons (eval (first-operand exps) env)
-            (list-of-values
+      (cons (actual-value
+             (first-operand exps)
+             env)
+            (list-of-arg-values
+             (rest-operands exps)
+             env))))
+
+(define (list-of-delayed-args exps env)
+  (if (no-operands? exps)
+      '()
+      (cons (delay-it
+             (first-operand exps)
+             env)
+            (list-of-delayed-args
              (rest-operands exps)
              env))))
 
 (define (eval-if exp env)
-  (if (true? (eval (if-predicate exp) env))
+  (if (true? (actual-value (if-predicate exp)
+                           env))
       (eval (if-consequent exp) env)
       (eval (if-alternative exp) env)))
 
@@ -348,15 +362,15 @@
 (define (apply-primitive-procedure proc args)
   (apply (primitive-implementation proc) args))
 
-(define input-prompt  ";;; M-Eval input:")
-(define output-prompt ";;; M-Eval value:")
+(define input-prompt  ";;; L-Eval input:")
+(define output-prompt ";;; L-Eval value:")
 
 (define (driver-loop)
   (prompt-for-input input-prompt)
   (let ((input (read)))
-    (let ((output
-           (eval input
-                 the-global-environment)))
+    (let ((output (actual-value
+                   input
+                   the-global-environment)))
       (announce-output output-prompt)
       (user-print output)))
   (driver-loop))
@@ -439,5 +453,33 @@
                                 (cdr orig-body))))))
 
   (traverse-body (list 'let '()) '() proc-body))
+
+(define (delay-it exp env)
+  (list 'thunk exp env))
+(define (thunk? obj) (tagged-list? obj 'thunk))
+(define (thunk-exp thunk) (cadr thunk))
+(define (thunk-env thunk) (caddr thunk))
+
+(define (evaluated-thunk? obj)
+  (tagged-list? obj 'evaluated-thunk))
+
+(define (thunk-value evaluated-thunk)
+  (cadr evaluated-thunk))
+
+(define (force-it obj)
+  (cond ((thunk? obj)
+         (let ((result
+                (actual-value
+                 (thunk-exp obj)
+                 (thunk-env obj))))
+           (set-car! obj 'evaluated-thunk)
+           ;; replace exp with its value:
+           (set-car! (cdr obj) result)
+           ;; forget unneeded env:
+           (set-cdr! (cdr obj) '())
+           result))
+        ((evaluated-thunk? obj)
+         (thunk-value obj))
+        (else obj)))
 
 (driver-loop)
