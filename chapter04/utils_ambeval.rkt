893367727cdd4c583e2f7f7f7eb7708aa52ff263
Jonathan Louis Kaplan <j.kiszle@gmail.com>
2022-08-23 16:51:56 +0100

    ex 4 44 done

--- chapter04/utils_ambeval.rkt
+++ chapter04/utils_ambeval.rkt
@@ -472,16 +472,16 @@
     (scan (frame-variables frame)
           (frame-values frame))))
 
-(define (b-and x y) (and x y))
-(define (b-or x y) (or x y))
+;; essential as `and` and `or` are
+;; racket special forms
+(define (band x y) (and x y))
+(define (bor x y) (or x y))
 
 (define primitive-procedures
   (list (list 'car car)
         (list 'cdr cdr)
         (list 'cons cons)
         (list 'null? null?)
-        (list 'and b-and)
-        (list 'or b-or)
         (list 'display display)
         (list 'newline newline)
         (list '+ +)
@@ -495,6 +495,8 @@
         (list 'not not)
         (list 'list list)
         (list 'apply metapply)
+        (list 'band band)
+        (list 'bor bor)
         ))
 
 (define (primitive-procedure-names)
